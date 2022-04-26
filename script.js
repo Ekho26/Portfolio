9409fe8a4e34803b6a1f1fb0e1b35f9efc6398a1
Ekho26 <munteanu.ionut1991@gmail.com>
2022-04-26 19:18:49 +0300

    changes

--- script.js
+++ script.js
@@ -2,40 +2,39 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-const mouseCircleFn = (x,y) => {
+const mouseCircleFn = (x, y) => {
   mouseCircle.style.cssText = `top:${y}px;left:${x}px; opacity:1`;
   mouseDot.style.cssText = `top:${y}px;left:${x}px; opacity:1`;
 };
 // End of Mouse Circle
 
 // Animated Circles
-const circles = document.querySelectorAll('.circle');
-const mainImg = document.querySelector('.main-circle img');
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
 
 let mX = 0;
 let mY = 0;
 const z = 50;
 
-const animateCircles=(e, x, y) =>{
-
-  if (x<mX){
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
     circles.forEach((circle) => {
       circle.style.left = `${z}px`;
     });
     mainImg.style.left = `${z}px`;
-  } else if (x>mX){
+  } else if (x > mX) {
     circles.forEach((circle) => {
       circle.style.left = `-${z}px`;
     });
     mainImg.style.left = `-${z}px`;
   }
 
-  if (y<mY) {
+  if (y < mY) {
     circles.forEach((circle) => {
       circle.style.top = `${z}px`;
     });
     mainImg.style.top = `${z}px`;
-  } else if (y>mY){
+  } else if (y > mY) {
     circles.forEach((circle) => {
       circle.style.top = `-${z}px`;
     });
@@ -44,15 +43,15 @@ const animateCircles=(e, x, y) =>{
 
   mX = e.clientX;
   mY = e.clientY;
-}
+};
 // End ofAnimated Circles
 
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
 
-  mouseCircleFn(x,y);
-  animateCircles(e,x,y);
+  mouseCircleFn(x, y);
+  animateCircles(e, x, y);
 });
 
 document.body.addEventListener("mouseleave", () => {
@@ -61,24 +60,40 @@ document.body.addEventListener("mouseleave", () => {
 });
 
 // Main Button
-const mainBtn = document.querySelector('.main-btn');
+const mainBtns = document.querySelectorAll(".main-btn");
 
-let ripple;
+mainBtns.forEach((btn) => {
+  let ripple;
 
-mainBtn.addEventListener('mouseenter', (e) => {
- const left = e.clientX - e.target
- .getBoundingClientRect().left;
- const top = e.clientY - e.target
- .getBoundingClientRect().top;
+  btn.addEventListener("mouseenter", (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
 
- ripple = document.createElement('div');
- ripple.classList.add('ripple');
- ripple.style.left = `${left}px`;
- ripple.style.top = `${top}px`;
- mainBtn.prepend(ripple);
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    btn.prepend(ripple);
+  });
+
+  btn.addEventListener("mouseleave", () => {
+    btn.removeChild(ripple);
+  });
 });
-
-mainBtn.addEventListener('mouseleave', ()=>{
-  mainBtn.removeChild(ripple);
-})
 // End of Main Button
+
+// About Me Text
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent =
+  "I am a designer& I create awards winning websites with the best user experience & I do not talk much, just contact me :)";
+
+Array.from(aboutMeTextContent).forEach((char) => {
+  const span = document.createElement("span");
+  span.textContent = char;
+  aboutMeText.appendChild(span);
+
+  span.addEventListener("mouseenter", (e) => {
+    e.target.style.animation = "aboutMeTextAnim 10s infinite";
+  });
+});
+// End of About Me Text
